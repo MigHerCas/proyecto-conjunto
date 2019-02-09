73cf1765d18ef4e6416198b2e2bdfa9809e7226d
Miguel Hernanz <migherdcas@gmail.com>
2019-02-09 13:23:38 +0100

    Primer test de conexion con BD.

---
+++ src/index.php
@@ -0,0 +1,23 @@
+<?php
+    require_once "./dist/init.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <!-- Falta escribir miembros del grupo -->
+    <meta name="author" content="" />
+    <meta name="author" content="" />
+    <meta name="author" content="" />
+    <meta name="author" content="" />
+    <title>Test</title>
+    <link rel="stylesheet" href="./styles/main.css" />
+</head>
+<body>
+    <h2>Prueba</h2>
+</body>
+</html>
